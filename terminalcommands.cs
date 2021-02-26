7aa67ab71617ccff7be4f79df0876e1735e05bec
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-26 13:15:23 +0300

    Create terminalcommands.cs

---
+++ terminalcommands.cs
@@ -0,0 +1,33 @@
+// public editor //
+    Main Class
+
+      public float="terminal"
+      public float="string"
+         GenerateCommand{ string + float
+         CommandName="AllServers"
+         GenerateCommand{ string + float
+         CommandName="CreateServer"
+ //Tokens//
+       CommandLink { string + float
+       CommandSource="AllServers"
+    string + { == float.host
+    impostor.server == {  string.id
+         AllServer.host
+    LinkKey= { + 0000000 { 
+      generateKey { = _this.key
+        ServerExtensions.{ Id
+ 
+     CommandLink { string + float
+     CommandSource="CreateServer"
+         string.impostor
+      GenerateServer.( = AmongTasksManager.exe
+      Server.impostor( = { string + float
+           data. { { = id  
+                }
+    createServer.id( = Tasks.float + string
+      float.host( = createServer
+          ipLocal = { host.Id }
+     
+    
+  
+  
