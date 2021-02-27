3bda7f825b968bf68705e72cbd854bbc0fe4ccfb
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-28 00:30:11 +0300

    Create info.cpp

---
+++ Servers/info.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <game>
+#include <server>
+   namespace.bat
+      string ( = < 0 )
+    string( = < 1 ( AmongUs.exe
+     open ( = x
+     open ( server.keys
+        file(system.id
+      open ( = x, z
+          servers.host( =  3 { Among us }
+          server.host( = 1 { server.ip }
+      open(name.json)
+         host.server ( = { 2 server.ip }
+      open.namespace.file == { file
+      jsonscript ( f, source.info
+          server.isHost("CreateGame")
+   
+    
+ 
+    
