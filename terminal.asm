9862143d3841c424867a0f68cb461f7d0729cbd2
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-28 00:18:48 +0300

    Create terminal.asm

---
+++ terminal.asm
@@ -0,0 +1,21 @@
+mov mX00000
+mov mx91848, i
+mov
+pop each, 855
+ bulidconfig:
+string mov
+mov byte [0X58847] ox2948
+mov bytes.config
+pop search [OX84638]
+game matrix [Ox9379]
+  call string  get among us
+  call open [OX9367] amongus.exe
+  mov bytes, open
+  mov bytes, [OX94629]
+  mov const [OX84728] processor
+  mov bytes [OX4726]
+  call game.strint
+  mov server.ip [OX8462]
+  mov open bytes, [OX83628]
+  mov ip [OX83628] ip.terminal
+  mov server, bytes, [OX83682]
