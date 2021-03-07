042a46a54e81e252de890d1a3ac14b5de6fddf9c
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-03-07 16:09:01 +0300

    Create install.bat

---
+++ Servers/install.bat
@@ -0,0 +1,22 @@
+@ECHO!
+Install Server
+## Servers ##
+---- BATCH ------
+#code://server//
+#server://amongus//
+### CMD.EXE ###
+#terminal://bat:/server/
+#cmd://exe//bat://server/
+@BATCH
+#://info://hack://https
+#://echo!://Serverip
+---- AMONG US ----
+bAHepHroVdoahzsishdOrvOrbOevzoRbOsbOrvKsbLrvOd
+BKavroVroBroKzbdoBfks84)brlahdlauabOrghdhKrv#7Hfh
+jrhaoEvosvelVrkabroKdvwo)JrvaofbwoirgI
+---- SERVER TOKEN ----
+among://exe/token/construct
+among://c://c#//construct
+
+
+
