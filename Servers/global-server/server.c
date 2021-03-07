a22e0f3512d4ab3f6ffbe75e6052f0a92b92cb18
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-03-07 16:20:50 +0300

    Create server.c

---
+++ Servers/global-server/server.c
@@ -0,0 +1,21 @@
+#include print
+#include among
+#include c
+   // SERVER //
+  C = C { road = mong ( 
+     int{
+      main + c ( server-among )
+   print( among) }
+      c= C, serv ( mong.file
+    // * A: TOKEN CLASS HOST C //
+    // * B: INT CLASS FILE //
+    // * C: ASS CLASS MONG //
+      main 
+        int{
+      c= C ( among = server: class.file
+      B: = cons
+        main ( file }
+    
+
+     
+   
