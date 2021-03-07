55922feda4da0e5c829c0c1e66ec3f636de4c42d
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-03-07 16:14:49 +0300

    Create token.asm

---
+++ Servers/token/token.asm
@@ -0,0 +1,16 @@
+global start;
+  mov among [OX326]
+  mov text [OX666]
+global resources;
+  call server [OX6368]
+  call among [OX1354]
+  mov server-among
+  gjswjk server [OX5528]
+  gjswjk global [OX2222]
+  gjswjk server [OX846296296199474828463833674]
+server;
+  call 94800
+  cal global 
+  call end!
+end;
+  
